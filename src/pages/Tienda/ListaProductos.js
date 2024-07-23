e6aea13d87d1a5677a226c88bea75d9088b11924
josdank <josue.guerra@epn.edu.ec>
2024-07-22 22:00:22 -0500

    Carrito1

--- src/pages/Tienda/ListaProductos.js
+++ src/pages/Tienda/ListaProductos.js
@@ -6,7 +6,7 @@ const ListaProductos = ({ agregarAlCarrito }) => {
         { id: 1, name: 'Mini-Pastel', price: 3.00, image: '/assets/img/mini-pastel.jpg' },
         { id: 2, name: 'Galletas', price: 1.00, image: '/assets/img/galletas.jpg' },
         { id: 3, name: 'Pastelitos', price: 2.00, image: '/assets/img/pastelitos.jpg' },
-        { id: 4, name: 'Empanadas', price: 1.00, image: '/assets/img/empanadas.jpg' }
+        { id: 4, name: 'Empanaditas', price: 1.00, image: '/assets/img/empanadas.jpg' }
     ];
 
     return (
