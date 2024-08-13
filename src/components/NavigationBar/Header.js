5063ffe746ac23e61ee5c5359fef03d5252ac800
Angel <151583275+Anng3l@users.noreply.github.com>
2024-08-12 19:35:51 -0500

    Navbar responsive

--- src/components/NavigationBar/Header.js
+++ src/components/NavigationBar/Header.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './estilo.css';
 import Logo from '../../assets/img/logoDoggift.png'
 import Icono from '../../assets/img/iconoDoggift.png'
+import HamburguesaMenu from '../../assets/img/hamburguesaMenu.png'
 
 const Header = ({ onShowProductList, onShowNosotros }) => {
+    const [scrolled, setHasScrolled] = useState(false);
+    const apply = 400; // Valor de scroll para aplicar la clase
+    const tolerance = 10; // Margen para evitar el flickering
+    let opcionesRef = useRef(null);
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+
+            if (scrollPosition > apply + tolerance) {
+                if (!scrolled) {
+                    setHasScrolled(true);
+                }
+            } else if (scrollPosition < apply - tolerance) {
+                if (scrolled) {
+                    setHasScrolled(false);
+                }
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [scrolled]);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        if (opcionesRef.current) {
+            opcionesRef.current.classList.toggle('desplegable');
+        }
+    };
+    
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (opcionesRef.current && !opcionesRef.current.contains(event.target)) {
+                opcionesRef.current.classList.remove('desplegable');
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     return (
-        <header className="header">
-            <div className='header__background'>
+        <header className='header'>
+            <div className={`header__background ${scrolled ? 'scrolled' : 'header__background'}`}>
                 <div className='head'>
                     <a href='https://twitter.com/' class="fa-brands fa-x-twitter"></a>
                     <a href='https://www.google.com/' class="fa-brands fa-tiktok"></a>
@@ -20,8 +68,8 @@ const Header = ({ onShowProductList, onShowNosotros }) => {
                     <a href='' className='logo'>
                         <img src={Logo} alt="Logo" className='doggiftLogo'></img>
                     </a>
-                    <div className='header__navbar--opciones'>
-                        <a>
+                    <div ref={opcionesRef} className='header__navbar--opciones'>
+                        <a className='homeA'>
                             <i class="fa-solid fa-house"></i>
                             <span className='header__navbar--opciones--nombre'>HOME</span>
                         </a>
@@ -44,18 +92,22 @@ const Header = ({ onShowProductList, onShowNosotros }) => {
                     </div>
 
                     <div className='header__navbar--busqueda'>
-                        <div>
+                        <div className='bloqueDos'>
                             <div className='header__navbar--busqueda--lupa'>
-                                <a class="fa-solid fa-magnifying-glass"></a>
+                                <a class="fa-solid fa-magnifying-glass" id='lupaNavbar'></a>
                                 <div className='linea'></div>
                             
-                                <a class="fa-solid fa-cart-shopping"></a>
+                                <a class="fa-solid fa-cart-shopping" id='carritoNavbar'></a>
                                 <div className='linea'></div>
                                 <div className='cantidades'>
                                     <div className='carrito_contador'>0.00</div>
                                     <div className='carrito_contador_items'>0</div>
                                     <div className='carrito_contador_itemsPalabra'>items</div>
                                 </div>
+                                <div className='lineaHamburguesa'></div>
+                                <a href="#" id='hamburguesaNavbar' onClick={handleClick}>
+                                    <img src={HamburguesaMenu} className='hamburguesaMenu'></img>
+                                </a>
                             </div>
                         </div>
                     </div>
--- src/pages/Nostros/nosotros.js
+++ src/pages/Nostros/nosotros.js
@@ -11,10 +11,10 @@ import Pastelillos from '../../assets/img/pastelillos.jpg'
 
 export const nosotros = () => {
   return (
-    <div>
+    <div className='NosotrosTotal'>
       <div className='nosotros'>
         <div className='quienesSomos'>
-          <img src={logo} alt="Logo" className='doggiftLogo'></img>
+          <img src={logo} alt="Logo" className='doggiftLogoNosotros'></img>
           <h2>¿Quiénes somos?</h2>
           <p> En Doggift, somos apasionados por la salud y el bienestar de tus mascotas. 
             El propósito de Doggift es el de ofrecer alternativas más saludables a las croquetas tradicionales, 
@@ -24,11 +24,11 @@ export const nosotros = () => {
             ingredientes naturales y libres de aditivos artificiales. Cada uno de nuestros alimentos está 
             cuidadosamente formulado para ofrecer una nutrición óptima, respetando los más altos estándares 
             de calidad y sostenibilidad. </p>
-            <img src={Tienda} className='tienda'></img>
+            <img src={Tienda} className='tiendaNosotros'></img>
         </div>
           
-        <div className='calidad'>
-          <img src={calidad} alt="Logo" className='calidadLogo'></img>
+        <div className='calidadNosotros'>
+          <img src={calidad} alt="Logo" className='calidadLogoNosotros'></img>
           <h2>Calidad</h2>
           <p> En Doggift, la calidad es <b> nuestro pilar fundamental </b>. Nos dedicamos a ofrecer alimentos orgánicos 
             y nutritivos para mascotas, utilizando únicamente ingredientes naturales y libres de aditivos 
@@ -36,8 +36,8 @@ export const nosotros = () => {
             para asegurar que proporcionamos solo lo mejor para la salud y el bienestar de las mascotas. </p>
         </div>  
           
-        <div className='alimentacionSalud'>
-          <img src={salud} alt="Logo" className='alimentacionLogo'></img>
+        <div className='alimentacionSaludNosotros'>
+          <img src={salud} alt="Logo" className='alimentacionLogoNosotros'></img>
           <h2>Alimentación y salud</h2>
           <p> La salud de las mascotas está directamente influenciada por lo que consumen diariamente. Una dieta 
             rica en nutrientes esenciales puede prevenir enfermedades, mejorar la energía y el estado de ánimo, y 
@@ -45,7 +45,7 @@ export const nosotros = () => {
             satisfacen el apetito de las mascotas, sino que también apoyan su bienestar general. Creemos que al 
             proporcionar alimentos de alta calidad, estamos invirtiendo en la salud futura de las mascotas, 
             asegurando que vivan vidas plenas y saludables. </p>
-            <div className='fotos'> 
+            <div className='fotosNosotros'> 
               <img src={Empanadas} className='empanadas'></img>
               <img src={Pastelillos} className='pastelillos'></img>
             </div>
